d75190d1aac15a998560ebbe89885c11c3c2e983
Nenad Brcic <neno@neno.ch>
2024-07-12 19:05:15 +0200

    Remove db from libs

--- libs/db/src/index.ts
+++
@@ -1,5 +0,0 @@
-export const dbconfig = 'dotenv.confg';
-export * from './entities';
-export * as CustomerInteractor from './interactors';
-
-export { customers as customersTestData } from './seeds/data/customers';
--- libs/db/src/interactors/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './customerInteractor';
-// export * from './customerInteractor.types';
