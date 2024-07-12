08b2a420f8fcb3b09f47d7f1eda1b23cf8a661e2
Nenad Brcic <neno@neno.ch>
2024-07-12 11:21:46 +0200

    Delete all libs

--- libs/db/src/entities/Customer.ts
+++
@@ -1,9 +0,0 @@
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
-import { z } from 'zod';
-import { customers } from '../schema/customers';
-
-export const Customer = createSelectSchema(customers);
-export type Customer = z.infer<typeof Customer>;
-
-export const InsertCustomer = createInsertSchema(customers);
-export type InsertCustomer = z.infer<typeof InsertCustomer>;
