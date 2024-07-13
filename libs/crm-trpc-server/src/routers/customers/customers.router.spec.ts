a45d88e92562ee6866f92c6c30a9184aea8d10f7
Nenad Brcic <neno@neno.ch>
2024-07-13 14:54:25 +0200

    Add customers router: all

--- libs/crm-trpc-server/src/routers/customers/customers.router.spec.ts
+++ libs/crm-trpc-server/src/routers/customers/customers.router.spec.ts
@@ -1,20 +1,29 @@
+import { customersTestData } from '../../db';
 import { trpc } from '../../trpc';
 import { customersRouter } from './customers.router';
+import { describe, it, expect } from 'vitest';
 
 const { createCallerFactory } = trpc;
 const createCaller = createCallerFactory(customersRouter);
 const caller = createCaller({ session: null });
 
-describe('customersRouter', () => {
+describe.only('customersRouter', () => {
   it('should exist', () => {
     expect(customersRouter).toBeTruthy();
   });
 
   it('should get all customers', async () => {
-    const input = { limit: 20, offset: 20 };
+    const input = {
+      limit: 20,
+      offset: 0,
+      orderBy: 'id' as const,
+    };
     const allCustomers = await caller.all(input);
 
     expect(allCustomers).toBeTruthy();
     expect(allCustomers.length).toBe(input.limit);
+    expect(allCustomers[input.limit - 1].id).toEqual(
+      customersTestData[input.limit - 1].id
+    );
   });
 });
--- libs/crm-trpc-server/src/routers/customers/customers.router.ts
+++ libs/crm-trpc-server/src/routers/customers/customers.router.ts
@@ -1,19 +1,24 @@
 import { z } from 'zod';
 import { trpc } from '../../trpc';
 import { CustomerInteractor } from '../../db';
+import { CustomerItemOrderBy } from '../../dto';
+// import { OrderByEnum } from '../../db/interactors/customerInteractor.types';
 
 export const customersRouter = trpc.router({
   all: trpc.procedure
     .input(
       z.object({
-        limit: z.number().optional().default(20),
-        offset: z.number().optional().default(0),
+        limit: z.number().default(20),
+        offset: z.number().default(0),
+        orderBy: CustomerItemOrderBy,
+        // order: OrderByEnum.optional().default(`asc`),
       })
     )
-    .query(({ input: { limit, offset } }) => {
+    .query(({ input: { limit, offset, orderBy } }) => {
       return CustomerInteractor.all({
         limit,
         offset,
+        orderBy,
       });
     }),
 });
