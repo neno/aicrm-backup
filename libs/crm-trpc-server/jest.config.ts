6591a48354c9a3723b42e371f9e8f11fdbfbea84
Nenad Brcic <neno@neno.ch>
2024-07-19 10:08:56 +0200

    Add jest-mock-extended

--- libs/crm-trpc-server/jest.config.ts
+++ libs/crm-trpc-server/jest.config.ts
@@ -3,6 +3,7 @@ export default {
   displayName: 'crm-trpc-server',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
